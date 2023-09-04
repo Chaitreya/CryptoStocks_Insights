9b5a6a16c4d0a9b2ee1b88b7e902ee9d0a9253d3
Chaitreya <cbhelkar55@gmail.com>
2023-09-04 12:06:40 +0530

    Intialized Backend and Frontend folders

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-center">
+        <h1 className='text-2xl'>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
